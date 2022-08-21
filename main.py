c77ee122fdd5a165aa51995147f8738d0692e5ac
iansind <108632228+iansind@users.noreply.github.com>
2022-08-20 22:11:19 -0500

    Update main.py
    Updated to use bot functionality for improved compartmentalization.

--- main.py
+++ main.py
@@ -1,79 +1,65 @@
 import os
 import discord
-import funcs
 from dotenv import load_dotenv
+from discord.ext import commands
+import funcs
 
 intents = discord.Intents.all()
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+DIONYSUS = commands.Bot(command_prefix='!', intents=intents)
 GUILD = os.getenv('DISCORD_GUILD')
-
-client = discord.Client(intents=intents)
 
 
 # Confirms connection to Discord.
-@client.event
+@DIONYSUS.event
 async def on_ready():
-    print(f'{client.user.name} has connected to Discord.')
+    print(f'{DIONYSUS.user.name} has connected to Discord!')
 
 
 # Prints a list of the guild members.
-@client.event
+@DIONYSUS.event
 async def on_ready():
-    guild = discord.utils.get(client.guilds, name=GUILD)
+    guild = discord.utils.get(DIONYSUS.guilds, name=GUILD)
 
     print(
-        f'{client.user} is connected to the following guild:\n'
+        f'{DIONYSUS.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
 
     members = '\n - '.join([member.name for member in guild.members])
+    print('Member count:', len(guild.members))
     print(f'Guild members:\n - {members}')
 
 
 # Welcomes new members to the guild by DM.
-@client.event
+@DIONYSUS.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(f'Hello, {member.name}, welcome to {GUILD}.')
 
 
-# Returns the top result from allrecipes.com for a requested dish.
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content[0:8] == '!recipe ':
-        url = funcs.allrecipes_search(message.content[8:])
-        await message.channel.send(url)
-    
-
-    elif message.content[0:6] == '!temp ':
-        temp = funcs.cook_temp(message.content[6:])
-        await message.channel.send(temp)
-
-# Performs various tasks in response to user queries.
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content[0:8] == '!recipe ':
-        url = funcs.allrecipes_search(message.content[8:])
-        await message.channel.send(url)
-
-    elif message.content[0:9] == '!isolate ':
-        instructions = funcs.isolate_instructions(message.content[9:])
-        for line in instructions:
-            await message.channel.send(line)
-
-    elif message.content[0:6] == '!temp ':
-        temp = funcs.cook_temp(message.content[6:])
-        await message.channel.send(temp)
-
-#client = CustomClient()
-client.run(TOKEN)
+@DIONYSUS.command(name='recipe',
+                  help='Enter a dish to receive a recipe URL from allrecipes.com.')
+async def recipe(ctx, arg):
+    url = funcs.allrecipes_search(arg)
+    await ctx.send(url)
 
 
+@DIONYSUS.command(name='isolate',
+                  help='Enter an allrecipes.com URL to receive the ingredient and instruction list in text format.')
+async def isolate(ctx, arg):
+    instructions = funcs.isolate_instructions(arg)
+    for line in instructions:
+        await ctx.send(line)
+
+
+@DIONYSUS.command(name='temp',
+                  help='Query what internal temperature a meat should be safely cooked to.')
+async def temp(ctx, arg):
+    cook_temp = funcs.cook_temp(arg)
+    await ctx.send(cook_temp)
+
+
+DIONYSUS.run(TOKEN)
